4d3a10087fe540368e040bd97efe2be79bf2d7c5
Vu Nguyen Ngoc <vudev98@gmail.com>
2024-08-31 19:11:47 +0700

    add css and path module

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+const path = require('path');
 const express = require('express');
 const bodyparser = require('body-parser');
 
@@ -7,13 +9,13 @@ const adminRouter = require('./routes/admin');
 const shopRouter = require('./routes/shop');
 
 app.use(bodyparser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRouter);
 app.use(shopRouter);
 
 app.use('/', (req, res, next) => {
-  res.status(404);
-  res.send('<h1>page not found</h1>');
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
---
+++ util/path.js
@@ -0,0 +1,4 @@
+const path = require('path'); // Import the path module at the beginning
+
+// Your existing code
+module.exports = path.dirname(require.main.filename);
